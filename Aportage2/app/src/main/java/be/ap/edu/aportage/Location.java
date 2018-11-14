15a7836ebe42ec51570e01921c71b080a10d2447
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-11-15 00:45:08 +0100

    creating test child firebase

---
+++ Aportage2/app/src/main/java/be/ap/edu/aportage/Location.java
@@ -0,0 +1,15 @@
+package be.ap.edu.aportage;
+
+public class Location {
+
+    public String name;
+    public String abbreviation;
+
+    public Location() {}
+
+    public Location(String name, String abbreviation) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
+
+}
--- Aportage2/app/src/main/java/be/ap/edu/aportage/MainActivity.java
+++ Aportage2/app/src/main/java/be/ap/edu/aportage/MainActivity.java
@@ -3,11 +3,24 @@ package be.ap.edu.aportage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Creating a new child in "locations" */
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Location location = new Location("test", "test");
+        String locationId = "test2";
+
+        mDatabase.child("locations").child(locationId).setValue(location);
     }
+
 }
