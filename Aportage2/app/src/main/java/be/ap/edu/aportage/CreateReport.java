dd84df3e74fde9700df4269a00d61b216f54b248
StijnDefour <defourstijn10@gmail.com>
2018-11-14 19:29:21 +0100

    QR code scanner complete
    add these lines to the build.gradle :
        implementation 'com.journeyapps:zxing-android-embedded:3.2.0@aar'
        implementation 'com.google.zxing:core:3.2.1'

---
+++ Aportage2/app/src/main/java/be/ap/edu/aportage/CreateReport.java
@@ -0,0 +1,57 @@
+package be.ap.edu.aportage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class CreateReport extends AppCompatActivity {
+
+    private Button qrButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_report);
+
+        qrButton = findViewById(R.id.qr_code_button);
+        final Activity activity = this;
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("Create Report", "Canceled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("Create Report", "Scanned");
+                Toast.makeText(this, "Scanned : " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
